67d0be5d64c99d906758b87810c2013d20c93389
jddeltoro <jdiazdeltoro@gmail.com>
2024-04-10 21:56:02 -0400

    add new cmp

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
+import { Card } from '@/app/ui/dashboard/cards';
+
+
 export default function Page() {
-    return <p>Customers Page</p>;
+    return (
+        <main>
+            <h1 className={`mb-4 text-xl md:text-2xl`}>
+                Customers
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Card title="Total Customers" value={0} type="customers" />
+            </div>
+        </main>
+    );
 }
