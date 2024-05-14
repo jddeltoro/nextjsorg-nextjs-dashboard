fc35d054a31148ec3863c9f625e2ca226ea18031
jddeltoro <jdiazdeltoro@gmail.com>
2024-05-13 20:54:47 -0400

    remove action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,15 +1,1 @@
 'use server';
-
-
-export async function createInvoice(formData: FormData) {
-    const response = await fetch('/api/invoices', {
-        method: 'POST',
-        body: formData,
-    });
-
-    if (!response.ok) {
-        throw new Error('Failed to create invoice');
-    }
-
-    return response.json();
-}
